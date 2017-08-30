3c2af420dbe711cb8b3be3ff37c5febd9694f096
Sven2905 <fuhrmann.sven@gmail.com>
2017-08-30 20:21:49 +0200

    Programm zum Erstellen von Testdaten für hnbkWetter

---
+++ Testdaten/Testdaten/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testdaten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Nicht perfekt, aber zum schnellen Erstellen von Testdaten reicht es...
+
+            TestingData data = new TestingData(50);
+            data.CreateTestData();
+        }
+    }
+}
